e72e11b77a118e24140d2774041e523bb3fa2f48
Devayush Rout <devayushrout@gmail.com>
2025-02-17 00:44:34 +0530

    LogisticRegression

---
+++ LogisticRegression.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def __init__(self, n_iters = 1000, lr = 0.001):
+    self.n_iters = n_iters
+    self.lr = lr
+    self.bias = bias
+    self.weights = weights
+    
+def 
+
+
+def predict():
