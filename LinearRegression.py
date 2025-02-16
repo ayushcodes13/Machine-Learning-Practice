e24384514e8bc3897586adc0560fbed0b9906468
Devayush Rout <devayushrout@gmail.com>
2025-02-16 10:51:15 +0530

    LinearRegression

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,7 +2,6 @@ import numpy as np
 
 class LinearRegression:
     
-    
     def __init__(self, lr = 0.001, n_iters = 1000):
     
     self.lr = lr
@@ -12,5 +11,10 @@ class LinearRegression:
     
     def fit(self, X, y): #x,y refers to the dataset that we will get from the user
     
-    n_samples
+    n_features, n_samples = X.shape #to know how many features and samples we are having in our dataset
+    #since we have only one feature in our dataset then only one weight is needed if there would have been more then we would need more number of weights
+    self.weights = np.zeroes(n_features) #created weights for each feature having initial value as 0
+    self.bias = 0
     
+    #we will use vectorizaton instead of calculating the y_pred for each point - it will help to predict the samples of all at the same time
+    y_pred = np.dot(X, self.weights) + self.bias
