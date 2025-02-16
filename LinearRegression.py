9d75c2d371167bd55db5730ba5dcfe0f8b4de971
Devayush Rout <devayushrout@gmail.com>
2025-02-16 10:38:43 +0530

    LinearRegression

---
+++ LinearRegression.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class LinearRegression:
+    def__init__(self, lr = 0.001, n_iters = 1000)
+    self.lr = lr
+    self.n_iters = n_iters
+    self.weights = None
+    self.bias = None
