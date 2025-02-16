5d00318b6280c994b7a21294c07832e93c585335
Devayush Rout <devayushrout@gmail.com>
2025-02-16 11:31:51 +0530

    LinearRegressionTest

--- LinearRegression.py
+++ LinearRegression.py
@@ -11,9 +11,9 @@ class LinearRegression:
     
     def fit(self, X, y): #x,y refers to the dataset that we will get from the user
     #here we are initialising the weights and bias ie 0
-        n_features, n_samples = X.shape #to know how many features and samples we are having in our dataset
+        n_samples, n_features = X.shape #to know how many features and samples we are having in our dataset
         #since we have only one feature in our dataset then only one weight is needed if there would have been more then we would need more number of weights
-        self.weights = np.zeros(n_features) #created weights for each feature having initial value as 0
+        self.weights = np.zeros((n_features,1)) #created weights for each feature having initial value as 0
         self.bias = 0
     
         for _ in range(self.n_iters): #looping through the number of iterations
@@ -22,7 +22,7 @@ class LinearRegression:
             y_pred = np.dot(X, self.weights) + self.bias #main formula for that line
             
             #gradient
-            dw = (1/n_samples) * np.dot(X, (y_pred-y)) #the dot thing also does the sum
+            dw = (1/n_samples) * np.dot(X.t, (y_pred-y).reshape(-1,1)) #the dot thing also does the sum and X.T ensures correct matrix multiplication
             db = (1/n_samples) * np.sum(y_pred-y)
             
             #gradient descent - finds the best value for w and b values
