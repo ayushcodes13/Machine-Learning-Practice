3d5fea8932c1bdc3e44d677e3e6a453044f1cf38
Devayush Rout <devayushrout@gmail.com>
2025-02-18 21:53:02 +0530

    LogisticRegression

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -2,9 +2,7 @@ import numpy as np
 
 
 def sigmoid(x):
-    
-    
-    
+    return 1/(1 + np.exp(-x))
     
 def __init__(self, n_iters = 1000, lr = 0.001):
     self.n_iters = n_iters
@@ -19,14 +17,13 @@ def fit(self,X,y):
     
     for _ im range(self.n_iters)
     
-    linear_pred = np.dot(X, self.weights) + self.bias
-    y_predicted = sigmoid(linear_pred)
+        linear_pred = np.dot(X, self.weights) + self.bias
+        y_predicted = sigmoid(linear_pred)
     
-    dw = 
-    db = 
+        dw = (1/n_samples) * np.dot(X.T, (y_predicted-y))
+        db = (1/n_samples) * np.sum(y_predicted-y)
     
-    
-    self.weights = self.weights - self.lr * dw
-    self.bias = self.bias - self.lr * db
+        self.weights = self.weights - self.lr * dw
+        self.bias = self.bias - self.lr * db
 
 def predict():
