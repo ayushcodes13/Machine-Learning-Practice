692f234ff91eaed4e2c95c84543e9c140deb313e
Devayush Rout <devayushrout@gmail.com>
2025-02-19 01:58:09 +0530

    knn

---
+++ knn.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class knn:
+    
+    def __init__ (self, k=3):
+        self.k = k
+        #k refers to the number of points we have to consider nearby
+        
+    def fit(self,X,y):
+        
+        self.X_train = X
+        self.y_train = y
+    
+    def predict (self,X):
+        #this function is for all the sum of the predictions
+        predictions = [self._predict(x) for x in X]
+        return predict        
+        
+    def _predict(self,X):
+        #this will calculate the predictions one by one manually
+        #step1 - calculate the distances using euclidean distance
